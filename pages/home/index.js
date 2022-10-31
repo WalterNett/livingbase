e695f32e399ab4a14b2576bc0e8e53a6de341cdd
WalterNett <106698809+WalterNett@users.noreply.github.com>
2022-10-30 22:14:58 -0300

    initial commit

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,1 +1,129 @@
-/* Desenvolva seu script aqui */
+class Postagens{
+
+
+    static renderPost (dados){
+
+        const sectioNoticias = document.querySelector('.posts')
+        const divNoticias = document.createElement("ul")
+        
+
+        dados.forEach(element => {
+            const noticias = document.createElement('li')
+            noticias.classList.add('noticias-card')
+            divNoticias.append(noticias)
+
+           this.criarPost(element, noticias)
+        });
+        sectioNoticias.appendChild(divNoticias)
+    }
+
+
+    static criarPost(data, pai ){
+
+                const fotoNoticia = document.createElement("img")
+                const divInfo = document.createElement('div')
+                    const tituloNoticia = document.createElement('h3')
+                    const descricaoNoticia = document.createElement('p')
+                    const noticiaCompleta = document.createElement('button')
+
+
+        fotoNoticia.src = data.image  
+        divInfo.classList.add('noticia-info')  
+        
+        tituloNoticia.innerText = data.title
+        descricaoNoticia.innerText = data.description
+        noticiaCompleta.innerText = 'Acessar conteúdo'
+        noticiaCompleta.classList.add('acessar-noticia')
+
+
+
+        noticiaCompleta.addEventListener('click', (event) =>{
+            event.preventDefault()
+            localStorage.setItem('idPost', data.id)
+            location.assign('../post/index.html')
+        })
+
+        pai.append(fotoNoticia, divInfo)
+        divInfo.append(tituloNoticia, descricaoNoticia, noticiaCompleta)
+    }
+
+}
+
+class requestPost {
+
+    static baseUrl = 'https://m2-api-living.herokuapp.com'
+    static headers = {
+        'Content-Type':"application/json",
+    }
+
+    static async posts(){
+
+        const getPost = await fetch(`${this.baseUrl}/news`,{
+            method:"GET",
+            headers: this.headers
+        })
+
+        .then(res => res.json())   
+        .then(res => {
+           Postagens.renderPost(res.news)
+           Category.renderPorCategory(res.news)
+           Category.renderAll(res.news)
+        })
+        .catch(err => console.log(err))
+
+    }
+
+}
+
+class Category {
+
+
+    static renderAll(dados){
+        const sectioNoticias = document.querySelector('.posts')
+        const btnAll = document.getElementById('todos')
+        btnAll.addEventListener('click', (event) =>{
+            sectioNoticias.innerHTML =' '
+            Postagens.renderPost(dados)
+        })
+    }
+
+
+    static renderPorCategory (dados){
+
+        const sectioNoticias = document.querySelector('.posts')
+        const buttons = document.querySelectorAll('.button')
+        let newArray = []
+
+        buttons.forEach(botoes =>{
+            botoes.addEventListener('click', (event) =>{
+                let newArray = []
+                event.preventDefault()
+                sectioNoticias.innerHTML = ''
+                dados.forEach(element => {
+                    if (botoes.id == element.category){
+                        
+                        newArray.push(element)    
+                        
+                    }
+
+                    
+                });
+                Postagens.renderPost(newArray)
+            })
+        })
+    }
+
+
+}
+
+function voltarAoTopo (){
+
+    const voltar = document.getElementById('voltar-top')
+    voltar.addEventListener('click', (event) =>{
+        event.preventDefault()
+        window.scrollTo(0,0)
+    })
+}
+
+requestPost.posts()
+voltarAoTopo ()
--- pages/post/index.js
+++ pages/post/index.js
@@ -1,1 +1,71 @@
-/* Desenvolva seu script aqui */
+class GetPost {
+
+
+    static baseUrl = 'https://m2-api-living.herokuapp.com'
+    static headers = {
+        'Content-Type':"application/json",
+    }
+    static postId = localStorage.getItem('idPost')
+
+    static async postID(){
+
+        const getPostId = await fetch(`${this.baseUrl}/news/${this.postId}`, {
+            method:'GET',
+            headers: this.headers
+        })
+
+        .then(res => res.json())
+        .then(res =>{
+            this.renderPost(res)
+            HomeRedirect.redirect(res)
+
+        })
+        .catch(error => console.log(error))
+    }
+
+
+    static renderPost(data){
+    
+        const main = document.querySelector('main')
+        const sectionPost = document.createElement('section')
+        const post = document.createElement('div')
+            const divTitle = document.createElement('div')  
+                const titulo = document.createElement('h2')
+                const desciption = document.createElement('p')
+            const imgPost = document.createElement('img')  
+            const contentNoticia = document.createElement('p')  
+
+        sectionPost.classList.add('postDiv')    
+        post.classList.add('post')
+        divTitle.classList.add('div-titulo')
+
+        titulo.innerText = data.title
+        desciption.innerText = data.description
+        imgPost.src = data.image
+        contentNoticia.innerText = data.content
+
+        post.append(divTitle, imgPost, contentNoticia)
+        divTitle.append(titulo, desciption)
+        sectionPost.appendChild(post)
+        main.appendChild(sectionPost)
+    }
+}
+
+
+
+GetPost.postID()
+
+class HomeRedirect {
+
+    static redirect (dados){
+
+        const homeBtn = document.getElementById('home')
+        homeBtn.addEventListener('click',(event) =>{
+            event.preventDefault()
+            localStorage.clear()
+            localStorage.setItem('category', dados.id)
+            location.assign('../home/index.html')
+        })
+
+    }
+}
